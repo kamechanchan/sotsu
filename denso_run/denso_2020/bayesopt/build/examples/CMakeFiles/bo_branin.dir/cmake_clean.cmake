4280349c3950cafb44edb612f50727d56b7762cc
ericlab <tsuchida.shinya413@mail.kyutech.jp>
2021-04-20 17:54:27 +0900

    seconid

---
+++ denso_run/denso_2020/bayesopt/build/examples/CMakeFiles/bo_branin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/bo_branin"
+  "../bin/bo_branin.pdb"
+  "CMakeFiles/bo_branin.dir/bo_branin.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bo_branin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ denso_run/denso_2020/bayesopt/build/examples/CMakeFiles/bo_camelback.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/bo_camelback"
+  "../bin/bo_camelback.pdb"
+  "CMakeFiles/bo_camelback.dir/bo_camelback.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bo_camelback.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
