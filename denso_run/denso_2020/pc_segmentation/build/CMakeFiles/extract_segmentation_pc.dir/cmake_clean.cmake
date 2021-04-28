1143a9517b887f31e44896964c69db7002bf3d19
ericlab <tsuchida.shinya413@mail.kyutech.jp>
2021-04-29 03:05:06 +0900

    nigate

---
+++ denso_run/denso_2020/pc_segmentation/build/CMakeFiles/extract_segmentation_pc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/extract_segmentation_pc.dir/src/extract_segmentation_pc.cpp.o"
+  "extract_segmentation_pc"
+  "extract_segmentation_pc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/extract_segmentation_pc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
