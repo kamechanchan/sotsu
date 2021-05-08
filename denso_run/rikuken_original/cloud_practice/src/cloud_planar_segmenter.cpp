4af98487d335e7daec1785292f36443d3004f1ee
ericlab <tsuchida.shinya413@mail.kyutech.jp>
2021-05-08 15:07:24 +0900

    suri

--- denso_run/rikuken_original/cloud_practice/src/cloud_planar_segmenter.cpp
+++ denso_run/rikuken_original/cloud_practice/src/cloud_planar_segmenter.cpp
@@ -80,7 +80,7 @@ int main(int argc, char ** argv)
 {
     ros::init(argc, argv, "cloud_planar_segmenter");
     ros::NodeHandle nh;
-    CloudOperationHandler handler(nh, new CloudPlanarSegmenter(nh), "/photoneo_center/sensor/points");
+    CloudOperationHandler handler(nh, new CloudPlanarSegmenter(nh), "cloud_downsampled");
     ros::spin();
     return 0;
 }
