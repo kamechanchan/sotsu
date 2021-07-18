7e9e3c04d3ba860ed8e7dca85fd75a8cd15e7c0c
ericlab <tsuchida.shinya413@mail.kyutech.jp>
2021-07-19 04:46:14 +0900

    ishiyama

--- denso_run/denso_pkgs/pose_estimator_pkg/trainer/models/layer/PointNet.py
+++ denso_run/denso_pkgs/pose_estimator_pkg/trainer/models/layer/PointNet.py
@@ -86,15 +86,15 @@ class STN3d(nn.Module):
     def forward(self, x):
         batchsize = x.size()[0]
         x = F.relu(self.bn1(self.conv1(x)))
-        print("x.shape",x)
+        print(x)
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
         x = torch.max(x, 2, keepdim=True)[0]
         x = x.view(-1, 1024)
-        #x = F.relu(self.bn4(self.fc1(x)))
-        x = F.relu(self.fc1(x))
-        #x = F.relu(self.bn5(self.fc2(x)))
-        x = F.relu(self.fc2(x))
+        x = F.relu(self.bn4(self.fc1(x)))
+        # x = F.relu(self.fc1(x))
+        x = F.relu(self.bn5(self.fc2(x)))
+        # x = F.relu(self.fc2(x))
         x = self.fc3(x)
 
         if self.quaternion:
