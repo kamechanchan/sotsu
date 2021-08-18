8d6b52ae3def810d17eada81e0b14aac42f90f38
shin77 <ishiyama.shin698@mail.kyutech.jp>
2021-08-18 16:20:44 +0000

    experiment

--- denso_run/rikuken_original/hdf5/visu_maskdata.py
+++ denso_run/rikuken_original/hdf5/visu_maskdata.py
@@ -4,6 +4,8 @@ import pcl.pcl_visualization
 import math
 import numpy as np
 
+from color_cloud_bridge.msg import out_segmentation
+
 
 cnt = 0
 c = 1
@@ -15,10 +17,11 @@ datapath = dataroot + dataset_name
 
 with h5py.File(datapath, mode="r") as f:
     pcl_data = f["data_1"]["masks"][()]
-    pcl_visu = pcl.PointCloud(pcl_data)
-    visual_after = pcl.pcl_visualization.CloudViewing()
-    visual_after.ShowMonochromeCloud(pcl_visu)
-    v = True
-    while v:
-        v = not(visual_after.WasStopped())
+    msg_out = out_segmentation()
+    for i in range(x_data.shape[0]):
+        for j in range(resolution):
+            msg_out.x.append(x_data[i][j][0])
+            msg_out.y.append(x_data[i][j][1])
+            msg_out.z.append(x_data[i][j][2])
+            msg_out.instance.append(pred_new[i][j])1
 
