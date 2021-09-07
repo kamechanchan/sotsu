9198954ed02a53626243e96a7ad922b0269193f0
shin77 <ishiyama.shin698@mail.kyutech.jp>
2021-09-07 00:46:31 +0000

    ish_dataset

--- denso_run/rikuken_original/hdf_file/src/visu_pcl_from_hdf5.py
+++ denso_run/rikuken_original/hdf_file/src/visu_pcl_from_hdf5.py
@@ -9,12 +9,12 @@ cnt = 0
 c = 1
 
 dataroot = "/home/ericlab/hdf5_file/"
-dataset_name = "instance_tsuchida_9_6_3_1.hdf5"
+dataset_name = "instance_tsuchida_9_7_4_1.hdf5"
 
 datapath = dataroot + dataset_name
 
 with h5py.File(datapath, mode="r") as f:
-    pcl_data = f["data_3"]["Points"][()]
+    pcl_data = f["data_2"]["Points"][()]
     pcl_visu = pcl.PointCloud(pcl_data)
     visual_after = pcl.pcl_visualization.CloudViewing()
     visual_after.ShowMonochromeCloud(pcl_visu)
