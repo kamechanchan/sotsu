01a82f7816919b99922c5b61c0766e3a42024fe0
tsuchidashinya <tsuchida.shinya413@mail.kyutech.jp>
2021-09-10 10:47:25 +0900

    ffd

--- denso_run/rikuken_original/pointcloud_usage/scripts/cloud_show.py
+++ denso_run/rikuken_original/pointcloud_usage/scripts/cloud_show.py
@@ -9,11 +9,7 @@ import sensor_msgs.point_cloud2 as pc2
 import time
 
 
-<<<<<<< HEAD
-file_name = rospy.get_param("~pcd_file_name", "/home/tsuchida/yes_1.pcd")
-=======
 file_name = "/home/ericlab/ros_package/denso_ws/src/denso_run/rikuken_original/annotation_package/pcd/save_6.pcd"
->>>>>>> 43a7e681882d064e4054e72304136882d89e48ea
 pcd = pcl.load(file_name)
 #pcd = pcl.load('/home/ericlab/1_/git_file_3/gpd/tutorials/table_mug.pcd')
 visual = pcl.pcl_visualization.CloudViewing()
